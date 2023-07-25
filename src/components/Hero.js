b119aaa47f3cd55d94587d5372f9d807587ca45c
harverino <sabrinapscampos@gmail.com>
2023-07-24 23:01:58 -0300

    Made it Responsive for Larger Devices/Monitors

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,14 +1,24 @@
 import React from "react";
-import ReactDOM from 'react-dom';
-import HeroIMG from '../img/heroimg.png'
+import ReactDOM from "react-dom";
+import HeroIMG from "../img/heroimg.png";
 
 export default function Hero() {
-    return (
-        <div className="hero">
-            <img src={HeroIMG} className="hero--image" alt="Hero Image"></img>
-            <h1>Get started your <br /> exciting <span>journey</span> <br />with us.</h1>
-            <h2>A Team of experienced tourism professionals will <br /> provide you with the best advice and tips for your <br /> desire place. </h2>
-            <button type="btn" className="btn">Discover Now</button>
-        </div>
-    )
+  return (
+    <div className="hero">
+      <div className="hero--text">
+        <h1>
+          Get started your <br /> exciting <span>journey</span> <br />
+          with us.
+        </h1>
+        <h2>
+          A Team of experienced tourism professionals will <br /> provide you
+          with the best advice and tips for your <br /> desire place.{" "}
+        </h2>
+        <button type="btn" className="btn">
+          Discover Now
+        </button>
+      </div>
+      <img src={HeroIMG} className="hero--image" alt="Hero Image"></img>
+    </div>
+  );
 }
