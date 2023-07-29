b602c7bb19334ab5b5fbf8d419c7757eaebb5dc9
harverino <sabrinapscampos@gmail.com>
2023-07-28 21:45:29 -0300

    Refactor: Refactored code for responsiveness across all devices and improved organization

--- src/components/BestVacationText.js
+++ src/components/BestVacationText.js
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Tree from "../img/tree.png";
 
 export default function TravelPackage() {
   return (
     <div className="centered-text">
       <div className="container">
-        <div className="container-title">
+        <div className="bvp--title">
           <h1>
             Best <span>vacation plan</span>
           </h1>
@@ -14,13 +13,6 @@ export default function TravelPackage() {
             Plan your perfect vacation with our travel agency. Choose among
             hundreds of all-inclusive offers!{" "}
           </p>
-        </div>
-        <div className="container-title-img">
-          <img
-            src={Tree}
-            className="vacation--plans--tree"
-            alt="Tree Img"
-          ></img>
         </div>
       </div>
     </div>
--- src/components/LatestBlogText.js
+++ src/components/LatestBlogText.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom";
 
 export default function LatestBlogText() {
   return (
-    <div className="centered-text">
-      <h1>
-        Get updated with <span>our latest post</span>
-      </h1>
+    <div className="latestblog-container">
+      <div className="latestblog-text">
+        <h1>
+          Get updated with <span>our latest post</span>
+        </h1>
+      </div>
     </div>
   );
 }
